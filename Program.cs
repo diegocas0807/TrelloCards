6a69cfd24174a16275abd3dcd9b2f112d3f2c37c
Xavier Llanga <xavierllanga@hotmail.com>
2018-12-07 19:10:54 +0100

    Homework 2 finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Manatee.Trello;
+using System.Threading.Tasks;
 
 namespace TrelloCards
 {
@@ -6,7 +8,31 @@ namespace TrelloCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            TrelloAuthorization.Default.AppKey = "";
+            TrelloAuthorization.Default.UserToken = "";
+
+            retrieveBoardCards().GetAwaiter().GetResult();
+
+
+            async Task retrieveBoardCards()
+            {
+                try
+                {
+                    var board = new Board("4d5ea62fd76aa1136000000c");
+                    await board.Refresh();
+                    Console.WriteLine($"All card of {board.Name}:");
+                    var cards = board.Cards;
+                    await cards.Refresh();
+                    foreach (Card card in cards)
+                        Console.WriteLine("· " + card.Name);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something was wrong " + e.Message);
+                }
+            }
         }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Homework 2
+
+To run code:
+
+```bash
+dotnet run
+```
+
+Output result sample:
+
+![output](./Capture.PNG)
